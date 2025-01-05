d69f0ebabe94b3f59ab951aa8edfec3914bfc94f
FrostyyShooter <devonweijand@hotmail.com>
2025-01-05 16:17:11 +0100

    worked on some styling for the aside. also implementing dark and light mode button in the nav bar

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	const accountMenu = document.querySelector(".account-list");
 	const category = document.getElementById("categoryButton");
 	const categoryMenu = document.querySelector(".category-menu");
+	const categorySvg = document.getElementById("categoryButtonIcon");
 
 	account.addEventListener("click", function (event) {
 		event.stopPropagation();
@@ -12,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	category.addEventListener("click", function (event) {
 		event.stopPropagation();
 		categoryMenu.classList.toggle("visible");
+		categorySvg.classList.toggle("rotate");
 	});
 
 	document.addEventListener("click", function () {
